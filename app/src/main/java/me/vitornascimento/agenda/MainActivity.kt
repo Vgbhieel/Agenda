6441cd7d1d1ce735177c9d1a4c0bc911046e2286
Vitor Nascimento <bielvitor2008@hotmail.com>
2020-12-19 23:23:39 -0300

    Implementado FAB e alterado o tipo de Activity da Main Activity

--- app/src/main/java/me/vitornascimento/agenda/MainActivity.kt
+++ app/src/main/java/me/vitornascimento/agenda/MainActivity.kt
@@ -1,11 +1,11 @@
 package me.vitornascimento.agenda
 
-import android.app.Activity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import me.vitornascimento.agenda.databinding.MainActivityBinding
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +13,7 @@ class MainActivity : Activity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setTitle("Lista de Alunos")
 
         val listaAlunos = arrayListOf("João", "José", "Maria", "Ana", "Paulo",
                                       "João", "José", "Maria", "Ana", "José",
