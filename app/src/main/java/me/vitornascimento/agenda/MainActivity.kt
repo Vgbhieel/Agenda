535f788468f301a801f81a1d5d54cccd80ee2d7f
Vitor Nascimento <bielvitor2008@hotmail.com>
2020-12-19 21:30:36 -0300

    Implementado layout usando View Binding

--- app/src/main/java/me/vitornascimento/agenda/MainActivity.kt
+++ app/src/main/java/me/vitornascimento/agenda/MainActivity.kt
@@ -2,9 +2,15 @@ package me.vitornascimento.agenda
 
 import android.app.Activity
 import android.os.Bundle
+import me.vitornascimento.agenda.databinding.MainActivityBinding
 
 class MainActivity : Activity() {
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
