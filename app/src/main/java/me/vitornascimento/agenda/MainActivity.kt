3ccf62938dd9c3ecebc6fce9f848d3ad6d832dc5
Vitor Nascimento <bielvitor2008@hotmail.com>
2020-12-19 23:05:49 -0300

    Implementado o ListView de Alunos e o iniciando o FAB

--- app/src/main/java/me/vitornascimento/agenda/MainActivity.kt
+++ app/src/main/java/me/vitornascimento/agenda/MainActivity.kt
@@ -2,6 +2,7 @@ package me.vitornascimento.agenda
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import me.vitornascimento.agenda.databinding.MainActivityBinding
 
 class MainActivity : Activity() {
@@ -12,5 +13,13 @@ class MainActivity : Activity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val listaAlunos = arrayListOf("João", "José", "Maria", "Ana", "Paulo",
+                                      "João", "José", "Maria", "Ana", "José",
+                                      "Maria", "Ana", "Paulo", "João", "José",
+                                      "Maria", "Ana")
+        val lvAlunos = binding.lvAlunos
+
+        lvAlunos.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaAlunos)
     }
 }
